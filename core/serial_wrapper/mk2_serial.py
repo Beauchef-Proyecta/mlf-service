72a61f0745829752dfe5db3e45b699629ca1ae79
Renaranananna <Renato.andaur@gmail.com>
2023-05-26 11:33:30 -0400

    feat: Add move server

---
+++ core/serial_wrapper/mk2_serial.py
@@ -0,0 +1,34 @@
+from .serial_controller import SerialController
+
+
+class MK2Serial:
+    
+    HEADER = 0xA0
+    CMD_JOINT = 0x10
+    CMD_BELT = 0x20
+    CMD_GRIPPER = 0x30
+    CMD_MAGNET = 0x40
+    CMD_PROXIMITY = 0xA0
+    CMD_LASER = 0xB0
+    
+    def __init__(self, port="/dev/ttyUSB0"):
+        self.serial = SerialController(port)
+        self.serial.open()
+    
+    def build_serial_msg(self, cmd: int, params: list):
+        data = [self.HEADER, len(params) + 1, cmd]
+        for p in params:
+            data.append(p)
+        return bytearray(data)
+    
+    def set_joints(self, angles: list):
+        data = self.build_serial_msg(self.CMD_JOINT, angles)
+        return self.serial.send_data(data)
+
+    def set_magnet_servo(self, angles: list):
+        data = self.build_serial_msg(self.CMD_GRIPPER, angles)
+        return self.serial.send_data(data)
+
+    def set_magnet_status(self, state: list):
+        data = self.build_serial_msg(self.CMD_MAGNET, state)
+        return self.serial.send_data(data)
---
+++ core/serial_wrapper/serial_controller.py
@@ -0,0 +1,26 @@
+import time
+
+from serial import Serial
+from serial.serialutil import SerialException
+
+class SerialController:
+    def __init__(self, port):
+        self.port = port
+        self.serial = None
+
+    def open(self):
+        try:
+            self.serial = Serial(self.port, 115200)
+            print("The port is available")
+        except SerialException:
+            print("The port is at use")
+            self.serial.close()
+            self.serial.open()
+
+    def close(self):
+        self.serial.close()
+
+    def send_data(self, data):
+        self.serial.write(data)
+        time.sleep(0.01)
+        return self.serial.read(2)
