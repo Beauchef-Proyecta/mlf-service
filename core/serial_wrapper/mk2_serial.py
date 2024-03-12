3cbb033bae757509143228925352f7d8fc711626
matiasguzmanp <matias.guzmanparra@gmail.com>
2024-03-12 11:20:50 -0300

    MG: update mk2_serial

--- core/serial_wrapper/mk2_serial.py
+++ core/serial_wrapper/mk2_serial.py
@@ -43,4 +43,4 @@ class MK2Serial:
     
     def set_gripper_servo(self, angle: list):
         data = self.build_serial_msg(self.CMD_GRIPPER, angle)
-        return self.serial.send_data(data)
+        return self.serial.send_data(data)   
