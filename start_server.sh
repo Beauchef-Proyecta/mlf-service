32b426bf047d385dff75ef80faa23572a5e2c631
matiasguzmanp <matias.guzmanparra@gmail.com>
2024-03-22 16:32:25 -0300

    MG: add start screen

---
+++ start_server.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+
+# Check that screen is running
+if screen -ls | grep -q "server" ;
+then
+  echo "Screen OK! "
+else
+  echo "Screen is NOT running!"
+  echo "Creating screen 'server'"
+  screen -dmS server
+  sleep 0.5
+fi
+
+# Check that server is running
+if curl -stderr /dev/null -I http://localhost:5000 | grep -1 "200 OK" ;
+then
+  echo "Server OK!"
+else
+  echo "Server is NOT running!"
+  screen -S server -X stuff  "workon mlf^M"
+  sleep 0.5
+  screen -S server -X stuff  "cd $HOME/mlf-service^M"
+  sleep 0.5
+  echo "Starting server..."
+  screen -S server -X stuff  "python core/server.py^M"
+  sleep 8
+  curl -I http://localhost:5000
+fi
+
+# Check that webcam is running
+if curl -stderr /dev/null -I http://localhost:8080 | grep -1 "200 OK" ;
+then
+ echo "Webcam OK!"
+else
+ echo "Webcam is NOT running!"
+ screen -S webcam -X stuff "workon mlf^M"
+ sleep 0.5
+ screen -S webcam -X stuff "cd $HOME/mlf-service^M"
+ sleep 0.5
+ echo "Starting webcam..."
+ screen -S webcam -X stuff "python core/webcam.py^M"
+ sleep 8
+ curl -I http://localhost:8080
+fi
+
+echo "done"
