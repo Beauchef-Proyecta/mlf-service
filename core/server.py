b3659b6e5580756e7d744f403e1224901d51d3ab
Renaranananna <Renato.andaur@gmail.com>
2024-07-01 16:20:35 -0400

    feat: add offset

--- core/server.py
+++ core/server.py
@@ -1,9 +1,13 @@
 import math
 import struct
 from flask import Flask, request, Response, Blueprint, make_response
-
+import os
 from serial_wrapper.mk2_serial import MK2Serial
 
+valor = os.getenv("MI_VARIABLE", '0,0,0,0')
+offset_list = [int(num) for num in valor.split(',')]
+
+print(offset_list)
 app = Flask(__name__)
 mk2_serial = MK2Serial()
 
@@ -29,10 +33,10 @@ def set_joints():
     q2 = request.args.get("q2")
     q3 = request.args.get("q3")
 
-    s0 = int(float(q0)) & 0xFF
-    s1 = int(float(q1)) & 0xFF
-    s2 = int(float(q2)) & 0xFF
-    s3 = int(float(q3)) & 0xFF
+    s0 = (int(float(q0)) + offset_list[0]) & 0xFF
+    s1 = (int(float(q1)) + offset_list[1]) & 0xFF
+    s2 = (int(float(q2)) + offset_list[2]) & 0xFF
+    s3 = (int(float(q3)) + offset_list[3]) & 0xFF
 
     mk2_serial.set_joints([s0, s1, s2, s3])
     return f"Mi nueva pose es: (q0={q0}, q1={q1}, q2={q2}, q3={q3})"
